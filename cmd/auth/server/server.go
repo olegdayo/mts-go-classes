8d327391f9f08faef04a1cfd9a24fa5b31be0ddf
Mmmr7 <ol-sidorenkov@mail.ru>
2022-12-30 21:37:50 +0300

    mongo

--- cmd/auth/server/server.go
+++ cmd/auth/server/server.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -16,22 +14,12 @@ const (
 
 type Server struct {
 	http.Server
-	Users map[string]*User `yaml:"users"`
 }
 
 func NewServer(port uint16) (s *Server) {
 	s = new(Server)
 	s.Addr = fmt.Sprintf(":%d", port)
-
 	s.Handler = setRouter()
-
-	s.Users = make(map[string]*User)
-	err := s.importUsers(PathToUsers)
-	if err != nil {
-		s.Users = make(map[string]*User)
-	}
-	fmt.Println(s.Users)
-
 	return
 }
 
@@ -41,21 +29,6 @@ func setRouter() *chi.Mux {
 	r.Get("/login", login)
 	r.Get("/verify", verify)
 	return r
-}
-
-func (s *Server) importUsers(path string) error {
-	bytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Printf("cannot open users file: %s\n", err.Error())
-		return err
-	}
-
-	err = yaml.Unmarshal(bytes, s)
-	if err != nil {
-		log.Printf("cannot import users: %s\n", err.Error())
-	}
-
-	return nil
 }
 
 func (s *Server) Start() {
